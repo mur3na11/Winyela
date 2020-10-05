45ccb506fbf89efbc825a0282f9334ed8e42dcaf
unknown <mur3na11@yahoo.com>
2020-10-05 17:27:09 +0200

    Initial

---
+++ js/main.js
@@ -0,0 +1,31 @@
+function initMap() {
+  const loc = { lat: 50.076771, lng: 15.921029 };
+  const map = new google.maps.Map(document.querySelector(".map"), {
+    zoom: 14,
+    center: loc
+  });
+  const marker = new google.maps.Marker({ position: loc, map: map });
+}
+window.addEventListener("scroll", function() {
+  if (window.scrollY > 150) {
+    document.querySelector("#navbar").style.opacity = 0.9;
+  } else {
+    document.querySelector("#navbar").style.opacity = 1;
+  }
+});
+
+// Smooth Scrolling
+$("#navbar a, .btn").on("click", function(event) {
+  if (this.hash !== "") {
+    event.preventDefault();
+
+    const hash = this.hash;
+
+    $("html, body").animate(
+      {
+        scrollTop: $(hash).offset().top - 100
+      },
+      800
+    );
+  }
+});
