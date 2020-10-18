886aa53212316c6bf11b65925092142c496961bc
Pavel Horcicka <pablo.alcudia@gmail.com>
2020-10-18 13:42:35 +0200

    animated pics_news

--- js/dogs.js
+++ js/dogs.js
@@ -1,1 +1,9 @@
-$('#lightgallery').lightGallery();
+const firstDog = document.querySelector('dog-card:first-child');
+const secondDog = document.querySelector('dog-card:nth-child(2)');
+const thirdDog = document.querySelector('dog-card:nth-child(3)');
+const fourthDog = document.querySelector('dog-card:nth-child(4)');
+const fifthDog = document.querySelector('dog-card:nth-child(5)');
+
+firstDog.addEventListener('hover', function (e) {
+
+})
