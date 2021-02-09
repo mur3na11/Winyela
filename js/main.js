573607bc3cae8ef4b641773ed6810e1f83867aee
Horčička <pho@pce.era.cz>
2021-02-10 00:57:01 +0100

    after a while3

--- js/main.js
+++ js/main.js
@@ -1,10 +1,16 @@
 function initMap() {
-  const loc = { lat: 50.076771, lng: 15.921029 };
-  const map = new google.maps.Map(document.querySelector(".map"), {
-    zoom: 14,
-    center: loc
+  // The location of Uluru
+  const uluru = { lat: -25.344, lng: 131.036 };
+  // The map, centered at Uluru
+  const map = new google.maps.Map(document.getElementById("map"), {
+    zoom: 4,
+    center: uluru,
   });
-  const marker = new google.maps.Marker({ position: loc, map: map });
+  // The marker, positioned at Uluru
+  const marker = new google.maps.Marker({
+    position: uluru,
+    map: map,
+  });
 }
 window.addEventListener("scroll", function() {
   if (window.scrollY > 150) {
