c3aa11a4440ea6e330405ae8c4cc0eb8b1c57a76
mur3na11 <mur3na11@yahoo.com>
2021-02-11 00:18:36 +0100

    after a while7

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ function initMap() {
   const uluru = { lat: 50.076729, lng: 15.921049 };
   // The map, centered at Uluru
   const map = new google.maps.Map(document.getElementById("map"), {
-    zoom: 8,
+    zoom: 10,
     center: uluru,
   });
   // The marker, positioned at Uluru
